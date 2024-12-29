0553a966b146fb3092955fcdd4d639ed23659981
ShreyVDesai <shreyvdesai@gmail.com>
2024-12-29 10:57:55 -0500

    Fixed scraper and changed textrank to textacy

--- textrank.py
+++ textrank.py
@@ -16,9 +16,17 @@
 #     return [phrase.text for phrase in doc._.phrases[:num_keywords]]
 import spacy
 import textacy
+import subprocess
 
 def preprocess_for_textrank(text):
-    nlp = spacy.load("en_core_web_sm")
+    model_name = "en_core_web_sm"  # Replace with the desired model name if needed
+    try:
+        
+        nlp = spacy.load(model_name)
+    except (ImportError, OSError):
+        subprocess.run(["python", "-m", "spacy", "download", model_name])
+        # os.system('python -m spacy download en_core_web_sm')
+        nlp = spacy.load(model_name)
     doc = nlp(text.lower())
     # Lemmatize and remove stopwords
     tokens = [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
@@ -26,7 +34,13 @@ def preprocess_for_textrank(text):
 
 def extract_keywords_textrank(text, num_keywords=10):
     text = preprocess_for_textrank(text)
-    nlp = spacy.load("en_core_web_sm")
+    model_name = "en_core_web_sm"  # Replace with the desired model name if needed
+    try:
+        nlp = spacy.load(model_name)
+    except (ImportError, OSError):
+        subprocess.run(["python", "-m", "spacy", "download", model_name])
+        # os.system('python -m spacy download en_core_web_sm')
+        nlp = spacy.load(model_name)
     
     # Process the text through spaCy and textacy
     doc = nlp(text)
